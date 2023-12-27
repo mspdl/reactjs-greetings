56c6841e541590b0e3ad17e037d2ff9c3a5ebded
Morgan <morgan.espindola@gmail.com>
2023-12-27 16:51:53 -0300

    My version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,25 @@ export default function Home() {
 
   const currentHour = new Date().getHours();
   let greetingMessage = "";
+  let backgroundColor = " from-sky-500 to-indigo-500";
 
   if (currentHour >= 5 && currentHour < 12) {
     greetingMessage = "Good morning 😊";
   } else if (currentHour >= 12 && currentHour < 18) {
     greetingMessage = "Good afternoon 😀";
+    backgroundColor = " from-orange-500 to-red-500";
   } else {
     greetingMessage = "Good evening 😴";
+    backgroundColor = " from-purple-500 to-black-500";
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+    <div
+      className={
+        "w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r" +
+        backgroundColor
+      }
+    >
       <div className="text-9xl">{fullTime}</div>
       <div className="text-5xl font-bold">{greetingMessage}</div>
     </div>
